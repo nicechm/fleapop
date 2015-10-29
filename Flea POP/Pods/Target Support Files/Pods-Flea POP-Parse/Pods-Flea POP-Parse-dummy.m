0882ede779d1213cd2c69b4513dc625fcd26d2e0
조현명 <root@worksmobile.com>
2015-10-29 10:17:52 +0900

    파일 정리.

--- Flea POP/Pods/Target Support Files/Pods-Flea POP-Parse/Pods-Flea POP-Parse-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Flea_POP_Parse : NSObject
-@end
-@implementation PodsDummy_Pods_Flea_POP_Parse
-@end
--- Flea POP/Pods/Target Support Files/Pods-Flea POP/Pods-Flea POP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Flea_POP : NSObject
-@end
-@implementation PodsDummy_Pods_Flea_POP
-@end
